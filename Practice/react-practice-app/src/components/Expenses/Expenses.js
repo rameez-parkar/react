0dbd6cf7bb9921f53786b714729033b46438aabc
rameez-parkar <rameezparkar2310@gmail.com>
2021-10-24 14:02:30 +0530

    conpleted rendering lists and conditional content section

--- Practice/react-practice-app/src/components/Expenses/Expenses.js
+++ Practice/react-practice-app/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import './Expenses.css';
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -18,6 +19,7 @@ const Expenses = (props) => {
     return (
         <Card className="expenses">
             <ExpensesFilter onFilterChange={filterChangeHandler} selectedYear={filteredYear} />
+            <ExpensesChart expenses={filteredExpenses} />
             <ExpensesList expenses={filteredExpenses} />
         </Card>
     );
